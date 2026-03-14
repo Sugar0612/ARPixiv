55db6d142239a82629889ebea8e1ec8f94652cfa
中下游 <287276293@qq.com>
2026-03-14 20:30:09 +0800

    feat: 添加点击人物出现粒子效果。

--- Assets/ARPixiv/Scripts/Toolkit/ModelClickHandler.cs
+++ Assets/ARPixiv/Scripts/Toolkit/ModelClickHandler.cs
@@ -1,10 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ModelClickHandler : MonoBehaviour
 {
+    public ParticleSystem TargetParticle;
+
+    private void Start()
+    {
+        TargetParticle.Stop();
+    }
+
     void OnMouseDown()
     {
         ModelTransformChange.Get().NeedChangeObject = gameObject;
+        StartCoroutine(ParticleCoroutine());
+    }
+
+    private IEnumerator ParticleCoroutine()
+    {
+
+        TargetParticle.Play();
+
+        yield return new WaitForSeconds(5.0f);
+
+        TargetParticle.Stop();
     }
 }
